7e9b46fb76f16e26a3fe1ae45593676b1476c883
Ian-McDowell-NCC <ian.mcdowell@student.northampton.edu>
2025-08-26 16:12:27 +0000

    submit

--- sample.cpp
+++ sample.cpp
@@ -1,7 +1,13 @@
-// YOUR NAME HERE
+// IAN McDOWELL
 #include <iostream>
 using namespace std;
 
 int main () {
-
+    int a, b;
+    cout << "Enter first integer: ";
+    cin >> a;
+    cout << "\nEnter second integer: ";
+    cin >> b;
+    cout << "\nThe sum is \n" << a+b;
+    return 0;
 }
